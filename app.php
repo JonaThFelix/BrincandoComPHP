20744ab7925d30ea004019ff20e94e2c1ec6e0ec
Jonathan Felix <123984244+JonaThFelix@users.noreply.github.com>
2025-02-26 22:56:58 -0300

    Update app.php

--- app.php
+++ app.php
@@ -10,3 +10,23 @@ function calc($peso,$altura):float{   // aqui criei uma função simples chamada
 $resposta = calc(1.80,80);            // Aqui faço o input dos valores definidos na função;
 echo $resposta;                       // Aqui trará a resposta da operação;
 ?>
+
+// FAZENDO A MESMA ATIVIDADE, MAS USANDO O NUMBER FORMAT PARA DEFINIR AS CASAS DECIMAIS
+<?php
+
+/* Atividade 3P Unibra - LPCS 2025.1
+Aluno: Jonathan José Felix Xavier 
+Matrícula: 2022252190
+Objetivo: Calcular IMC
+*/
+
+function imc ($peso,$altura){
+    $resultado = $peso / ($altura ** 2);
+    return $resultado;
+}
+
+$resposta = imc (80,1.80);
+    $formatado = number_format($resposta,2);
+    echo "O Imc foi: $formatado.";
+
+?>
